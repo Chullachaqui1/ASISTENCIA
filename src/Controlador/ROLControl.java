bf257ba6893a69733e69eeebf1505360426efb22
Antony <Antony@DESKTOP-3REM6DQ>
2024-10-10 10:54:34 -1200

    Mi aporte

--- src/Controlador/ROLControl.java
+++ src/Controlador/ROLControl.java
@@ -1,43 +1,19 @@
-package Controlador;
+package controlador;
 
 import Datos.RolDAO;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.table.DefaultTableModel;
 import Modelo.ROL;
 
-/**
- *
- * @author A07
- */
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ROLControl {
-    private final RolDAO DATOS;
-    private ROL obj;
-    private DefaultTableModel modeloTabla;
-    public int registrosMostrados;
-    
-    public ROLControl(){
-        this.DATOS=new RolDAO();
-        this.obj=new ROL();
-        this.registrosMostrados=0;
+    private RolDAO rolDAO;
+
+    public ROLControl() {
+        rolDAO = new RolDAO(); 
     }
-    
-    public DefaultTableModel listar(){
-        List<ROL> lista=new ArrayList();
-        lista.addAll(DATOS.listar());
-        
-        String[] titulos={"Nombre"};
-        this.modeloTabla=new DefaultTableModel(null,titulos);        
-        
-        String[] registro = new String[1];
-        
-        this.registrosMostrados=0;
-        for (ROL item:lista){
-            
-            registro[0]=item.getNombre();
-            this.modeloTabla.addRow(registro);
-            this.registrosMostrados=this.registrosMostrados+1;
-        }
-        return this.modeloTabla;
+
+    public List<ROL> listar() {
+        return rolDAO.listar();
     }
 }
